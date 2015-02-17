68b4f7d0ba9378c35f545725ea0ec93dec68126e
bstaykov <boycho.staykov@gmail.com>
2015-02-17 23:13:38 +0200

    RankingLoader Added

---
+++ TeamWorkGame/GameObjects/Ranking.cs
@@ -0,0 +1,14 @@
+﻿namespace TeamWorkGame.GameObjects
+{
+    using System.Collections.Generic;
+
+    public class Ranking
+    {
+        public Ranking()
+        {
+            this.TopPlayers = new Dictionary<string, int[]>();
+        }
+
+        public Dictionary<string, int[]> TopPlayers { get; set; }
+    }
+}
