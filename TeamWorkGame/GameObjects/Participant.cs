4a5c3e0462106d1b816521b34315482e6fba786d
bstaykov <boycho.staykov@gmail.com>
2015-02-18 13:06:06 +0200

    Ranking Save Optimization

--- TeamWorkGame/GameObjects/Participant.cs
+++ TeamWorkGame/GameObjects/Participant.cs
@@ -4,17 +4,17 @@
 
     public class Participant
     {
-        public Participant(string name, int level, int score)
+        public Participant(string name, int level, int moves)
         {
             this.Name = name;
             this.Level = level;
-            this.Score = score;
+            this.Moves = moves;
         }
 
         public string Name { get; set; }
 
         public int Level { get; set; }
 
-        public int Score { get; set; }
+        public int Moves { get; set; }
     }
 }
