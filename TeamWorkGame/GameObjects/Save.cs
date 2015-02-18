3ec1d4ce6e1367194f599a7e87c41945568eea9a
bstaykov <boycho.staykov@gmail.com>
2015-02-18 20:21:37 +0200

    SaveManager, add new save, load save

---
+++ TeamWorkGame/GameObjects/Save.cs
@@ -0,0 +1,13 @@
+﻿namespace TeamWorkGame.GameObjects
+{
+    using System;
+
+    public class Save
+    {
+        public char[,] Matrix { get; set; }
+
+        public int Level { get; set; }
+
+        public int Moves { get; set; }
+    }
+}
