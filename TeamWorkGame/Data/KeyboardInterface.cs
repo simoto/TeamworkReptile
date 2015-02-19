5de0ace99a2754054e285cef60124994fdc4c2c1
bstaykov <boycho.staykov@gmail.com>
2015-02-19 13:15:20 +0200

    KeyboardInterface keys

--- TeamWorkGame/Data/KeyboardInterface.cs
+++ TeamWorkGame/Data/KeyboardInterface.cs
@@ -7,8 +7,42 @@
     {
         public void ProcessInput(ConsoleKeyInfo key)
         {
-            //TODO: Must be implemented
-            throw new System.NotImplementedException();
+            if (key.Key == ConsoleKey.LeftArrow)
+            {
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.UpArrow)
+            {
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.S)
+            {
+                // save and exit
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.L)
+            {
+                //load
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.R)
+            {
+                // restart, start from same level
+                throw new NotImplementedException();
+            }
+            else if (key.Key == ConsoleKey.E)
+            {
+                // end game, start from level 1
+                throw new NotImplementedException();
+            }
         }
     }
 }
