ec57859a7d861dfa21967e153a7e41c8021209c8
bstaykov <boycho.staykov@gmail.com>
2015-03-02 19:14:45 +0200

    just commit untracked files :)

---
+++ TeamWorkGame/Interfaces/IStorage.cs
@@ -0,0 +1,11 @@
+﻿namespace TeamWorkGame.Interfaces
+{
+    using TeamWorkGame.GameObjects;
+
+    public interface IStorage
+    {
+        void Save(Save save);
+
+        Save Load(string userName, string password);
+    }
+}
